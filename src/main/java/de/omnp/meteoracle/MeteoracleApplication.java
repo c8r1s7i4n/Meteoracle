19febb816c332567eb62d60760b322f0fab78e83
c8r1s7i4n <b.christian98@proton.me>
2026-02-25 15:44:46 +0100

    Spring initialized

---
+++ src/main/java/de/omnp/meteoracle/MeteoracleApplication.java
@@ -0,0 +1,13 @@
+package de.omnp.meteoracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/omnp/meteoracle/MeteoracleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.omnp.meteoracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
